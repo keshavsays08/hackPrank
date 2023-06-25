e1cd7362f4ff3ccc414677c7abd8739c308effb1
keshavsays08 <137108477+keshavsays08@users.noreply.github.com>
2023-06-25 15:02:08 +0530

    added script.js

---
+++ scriptSana.js
@@ -0,0 +1,133 @@
+header.innerHTML = "using setTimeout"
+
+setTimeout(()=>{
+    division1.innerHTML = "hacking user social media handles data... please wait...\n"
+},1000)
+setTimeout(()=>{
+    division2.innerHTML = "using asynchronous data mining... please wait...\n"
+},1500)
+setTimeout(()=>{
+    division3.innerHTML = "fetching username & password... please wait..."
+},2000)
+setTimeout(()=>{
+    division4.innerHTML = "hacking *Abhinav Vyas*'s Instagram(if available) ID... please wait..."
+},4500) 
+setTimeout(()=>{
+    division5.innerHTML = "hacking *Abhinav Vyas*'s Facebook(if available)ID... please wait..."
+},5000)
+setTimeout(()=>{
+    division6.innerHTML="hacking *Abhinav Vyas*'s Twitter(if available)ID... please wait..."
+},6500)
+setTimeout(()=>{
+    division7.innerHTML = "hacking *Abhinav Vyas*'s Snapchat(if available)ID... please wait..."
+},7500)
+setTimeout(()=>{
+    division8.innerHTML = "hacking *Abhinav Vyas*'s Google+(if available)ID... please wait..."
+},8550)
+setTimeout(()=>{
+    division9.innerHTML = "hacking *Abhinav Vyas*'s WeChat(if available)ID... please wait..."
+},9600)
+setTimeout(()=>{
+    division10.innerHTML = "hacking *Abhinav Vyas*'s LinkedIn(if available)ID... please wait..."
+},10650)
+setTimeout(()=>{
+    division11.innerHTML = "hacking *Abhinav Vyas*'s Messenger(if available)ID... please wait..."
+},11650)
+setTimeout(()=>{
+    division12.innerHTML = "hacking *Abhinav Vyas*'s WhatsApp(if available)ID... please wait..."
+},12650)
+setTimeout(()=>{
+    division13.innerHTML = "hacking *Abhinav Vyas*'s Youtube(if available)ID... please wait..."
+},13650)
+
+setTimeout(()=>{
+    division14.innerHTML = "hacking *Abhinav Vyas*'s Vimeo(if available)ID... please wait..."
+},14650)
+ 
+
+try{
+console.log(rahul)
+}
+catch(error){
+    console.log(error)
+}
+//this thing doesn't stops the console interpretation 
+// rahul is nowhere but we are still logging it so it will through some error
+//but we just want no error to be here though we can accept the console.log() message we are okay with it 
+//so we do try and catch thing to try the error once again it may run this time and then if it is not able to run then we catch it and error doesn't get displayed 
+
+
+
+setTimeout(()=>{
+    division15.innerHTML = "processing... please wait..."
+},15660)
+setTimeout(()=>{
+    division16.innerHTML = "processing... please wait..."
+},16670)
+setTimeout(()=>{
+    division17.innerHTML = "processing... please wait..."
+},17680)
+setTimeout(()=>{
+    division18.innerHTML = "processing... please wait..."
+},18690)
+
+setTimeout(()=>{
+    division19.innerHTML ="username & password of *Abhinav Vyas* is now fetched from some handles namely... please wait..."
+},19000)
+
+setTimeout(()=>{
+    division20.innerHTML ="Instagram, LinkedIn,  Youtube, Whatsapp... please wait..."
+},20500)
+
+setTimeout(()=>{
+    division21.innerHTML ="loading array of data fetched... please wait..."
+},20700)
+setTimeout(()=>{
+    division24.innerHTML ="sending array of data fetched to server... please wait..."
+},20800)
+setTimeout(()=>{
+    division25.innerHTML ="array of data fetched have been received... please wait..."
+},20900)
+setTimeout(()=>{
+    division26.innerHTML ="filtering array of data fetched... please wait..."
+},30600)
+setTimeout(()=>{
+    division27.innerHTML ="applying xylem algo... array of data fetched... please wait..."
+},31600)
+
+
+
+
+
+
+
+
+
+
+
+
+setTimeout(()=>{
+    division22.innerHTML ="loading ... please wait..."
+},10000)
+setTimeout(()=>{
+    division23.innerHTML ="*********************************"
+},40700)
+
+setInterval(()=>{
+    header.innerHTML = "handling error using extention zyro.ai"
+},2000)
+
+setInterval(()=>{
+    header.innerHTML = "using third-party extention"
+},4000)
+
+setInterval(()=>{
+    footer.innerHTML="converting data for mining purpose"
+},5000)
+
+setInterval(()=>{
+    footer.innerHTML="again converting data for mining purpose"
+},2000)
+
+
+
